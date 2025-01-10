c2d8709c5351e1eea254122f2be44ea342e7e7b9
Arafat01007 <arafatfahadbro@gmail.com>
2025-01-10 15:26:23 +0600

    added  seat function

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 // seat btn selection
 const seats = document.getElementsByClassName('btn-seat');
 let clickCount = 0;
+let totalPrice = 0;
+
 
 for (const seat of seats) {
     seat.addEventListener('click', function () {
@@ -15,6 +17,7 @@ for (const seat of seats) {
         } else {
             alert('You can only select up to 4 seats.');
         }
+
         const seatCountElement = document.getElementById('seat-count');
         const defaultSeatCount = 40;
         const seatCount = defaultSeatCount - clickCount;
@@ -22,25 +25,45 @@ for (const seat of seats) {
         const seatNum = document.getElementById('seat-num');
         seatNum.innerText = clickCount;
         seatNum.classList.remove('hidden');
+        const table = document.getElementById('table')
+        // c1 economy 550tk
+        const seatId = seat.innerText;
+        const row = document.createElement('tr');
+        const cell1 = document.createElement('td');
+        const cell2 = document.createElement('td');
+        const cell3 = document.createElement('td');
+        cell1.innerText = seatId;
+        cell2.innerText = 'Economy';
+        cell3.innerText = '550tk';
+        row.appendChild(cell1);
+        row.appendChild(cell2);
+        row.appendChild(cell3);
 
-      
+        // deselect 
+        if (seat.classList.contains('bg-[#1dd100]')) {
+            table.appendChild(row);
+        } else {
+            const rows = table.getElementsByTagName('tr');
+            for (const r of rows) {
+                if (r.firstChild.innerText === seatId) {
+                    table.removeChild(r);
+                    break;
+                }
+            }
+        }
 
-        const t = document.createElement('tr');
-        t.innerHTML = `
-            <th class = "seat-id"></th>
-            <th id="seat-type" class="">Economoy</th>
-            <th id="seat-priec" class="flex justify-end"></th>
-        `;
-        const table = document.getElementById('table');
-        table.appendChild(t);
-          // seat payment
-        const seatId = document.getElementsByClassName('seat-id');
-        const seatInner = seat.innerText;
-        seatId.innerText = seatInner;
-        console.log(seatId);
         if (clickCount === 0) {
             seatNum.classList.add('hidden');
             seatId.innerText = '';
         }
+
+        const totalPriceElement = document.getElementById('total');
+        let totalPriceText = cell3.innerText;
+        let totalPriceInt = parseInt(totalPriceText.replace('tk', ''));
+        totalPrice += totalPriceInt;
+        totalPriceElement.innerText = totalPrice + 'tk';
+        console.log(totalPrice);
+
+
     });
 }
